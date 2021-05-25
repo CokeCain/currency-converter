fa430ee065f70cfc1b11dfe4a082a4be991b7524
CokeCain <cbabic@pfst.hr>
2021-05-25 11:58:32 +0200

    CORS Fix

--- js/model.js
+++ js/model.js
@@ -9,7 +9,7 @@ export const state = {
 
 export const getCurrencyCodes = async function () {
   try {
-    const data = await fetch(`http://api.exchangeratesapi.io/v1/latest?access_key=8095a2f21ff578a2a3297f33c3687ca5`);
+    const data = await fetch(`https://api.exchangeratesapi.io/v1/latest?access_key=8095a2f21ff578a2a3297f33c3687ca5`);
     const json = await data.json();
     populateRates(json);
   } catch (err) {
