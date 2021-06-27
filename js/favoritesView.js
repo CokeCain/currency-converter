62b584435597d6ae10f4ecada5e8f364e7901be7
CokeCain <cbabic@pfst.hr>
2021-06-27 19:39:16 +0200

    Some style fixes

--- js/favoritesView.js
+++ js/favoritesView.js
@@ -4,6 +4,9 @@ class FavoritesView extends View {
   _parentElement = document.querySelector('.favorites');
 
   _generateMarkup() {
+    console.log(this._data);
+    if (this._data.length > 0) document.querySelector('.sub-title').style.display = 'block';
+    else document.querySelector('.sub-title').style.display = 'none';
     return this._data.map(cur => this._markup(cur)).join('');
   }
 
